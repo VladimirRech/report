738696e7066c72944cd1ea37bbf0266529264a14
Vladimir Rech <vladimir@forlogic.net>
2018-12-17 14:25:04 -0200

    Sending files to repository.

---
+++ Stimulsoft2018/Stimulsoft2018/Form1.cs
@@ -0,0 +1,26 @@
+﻿using Stimulsoft.Report;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stimulsoft2018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rpt = new StiReport();
+            rpt.DesignWithWpf();
+        }
+    }
+}
