de3573821a416e94d80afeae5eef86ad2db466bf
Vladimir Rech <rechvladimir@hotmail.com>
2020-12-03 20:56:09 -0300

    Enviando exemplo de uso do SDK da Mundi Pagg

---
+++ mundi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MundiAPI.Standard;
+using MundiAPI.Standard.Models;
+
+///
+/// Programa demonstra como usar o sdk da Mundi Pagg
+/// Este código está disponível em: https://docs.mundipagg.com/docs/criando-uma-cobran%C3%A7a
+/// Este projeto foi criado com o Dotnet Core 5, Ubuntu e VsCode
+///
+
+namespace mundi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string basicAuthUserName = "sk_test_4AdjlqpseatnmgbW";
+            string basicAuthPassword = "";
+
+            var client = new MundiAPIClient(basicAuthUserName, basicAuthPassword);
+
+            var request = new CreateChargeRequest() {
+                Amount = 1490,
+                CustomerId = "cus_aGvrM1lCvxUKW9N7",
+                Payment = new CreatePaymentRequest() {
+                    PaymentMethod = "credit_card",
+                    CreditCard = new CreateCreditCardPaymentRequest(){
+                        CardId = "card_qBkxRKtmEfgyv1Zd",
+                        Card = new CreateCardRequest {
+                            Cvv = "353",
+                        }
+                    }
+                }
+            };
+
+            var response = client.Charges.CreateCharge(request);
+            Console.WriteLine("Done!");
+        }
+    }
+}
